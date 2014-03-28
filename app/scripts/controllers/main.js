22fd5bbb9364b65cdee51dbcaacbe6cb9ec53a90
Sander Struijk <sander.struijk@gmail.com>
2014-03-28 03:20:10 +0100

    initial prototyping of the client side code with angular.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,50 @@
 'use strict';
 
 angular.module('websyncApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    .controller('MainCtrl', function ($scope) {
+
+        function createFlag(name, short, tooltip) {
+            return {
+                name: name,
+                short: short,
+                tooltip: tooltip
+            };
+        }
+
+        $scope.tasks = [
+            {
+                id: 1,
+                name: 'Task1',
+                source: '/home/username',
+                destination: 'username@another.server.com:/backup',
+                flags: ['a', 'v', 'z'],
+                shell: 'ssh'
+            },
+            {
+                id: 2,
+                name: 'Task2',
+                source: '/usr/local/bin/fantasy',
+                destination: 'username@another.server.com',
+                flags: ['n'],
+                shell: 'bash'
+            },
+            {
+                id: 3,
+                name: 'Task3',
+                source: '/var/logs',
+                destination: 'username@another.server.com',
+                flags: ['n'],
+                shell: 'powershell'
+            }
+        ];
+
+        $scope.metadata = {
+            flags: [
+                createFlag('archive', 'a', 'archive mode; equals -rlptgoD (no -H,-A,-X)'),
+                createFlag('verbose', 'v', 'increase verbosity'),
+                createFlag('compress', 'z', 'compress file data during the transfer'),
+            ],
+            shells: ['ssh', 'bash', 'powershell']
+        };
+
     });
-  });
