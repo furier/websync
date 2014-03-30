199f3a94618340a90e5f4e9522e894f8464d3e0b
Sander Struijk <sander.struijk@gmail.com>
2014-03-31 00:25:40 +0200

    added a skeleton for a scheduler directive

---
+++ app/scripts/directives/scheduler.js
@@ -0,0 +1,12 @@
+/**
+ * Created by furier on 30/03/14.
+ */
+'use strict';
+
+angular.module('websyncApp')
+
+    .directive('scheduler', function(){
+        return {
+            restrict: 'E'
+        };
+    });
